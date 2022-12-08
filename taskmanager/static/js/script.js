afcf095ea3658bd836a62ec456db6d998a01bad4
Grainne Donegan <grainnejdonegan@gmail.com>
2022-12-08 22:35:23 +0000

    create base template to inherit for tasks in task manager

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialisation
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
